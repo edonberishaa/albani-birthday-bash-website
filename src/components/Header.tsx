a875c6c31dc40be8dd289af88f63c9ddfe86e638
Edon Berisha <150199937+edonberishaa@users.noreply.github.com>
2025-04-11 22:07:12 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,12 +13,12 @@ const Header = () => {
       <h1 
         className={`text-5xl md:text-6xl lg:text-7xl font-bold mb-4 shiny-text transition-opacity duration-1000 ${isMounted ? 'opacity-100' : 'opacity-0'}`}
       >
-        Gëzuar Ditëlindjen Albani!
+        Urime ditlindja vllau jone!
       </h1>
       <p 
         className={`text-xl md:text-2xl text-gray-300 transition-opacity duration-1000 delay-300 ${isMounted ? 'opacity-100' : 'opacity-0'}`}
       >
-        Shumë vite të lumtura, shok i ngushtë!
+        Edhe 100 vite tjera t lumtura!
       </p>
     </header>
   );
